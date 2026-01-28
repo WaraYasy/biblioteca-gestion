8ee03c99a6d2be46e02e4c8374b4273250c9b4c3
arantxa <149501497+arantxaMain@users.noreply.github.com>
2026-01-28 10:55:49 +0100

    DOCS: añadir organización de github en manifest

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     """,
 
     'author': "Arantxa-Wara",
-    'website': "https://www.yourcompany.com",
+    'website': "https://github.com/Nimbus-Develop",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
